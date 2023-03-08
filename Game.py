13ad1083a88d12cceb94e5b5773d35c5b7c52f65
Elias Theis <eliastheis.sg@gmail.com>
2023-03-08 09:40:02 +0100

    Add "super-objects" and some more events

--- Game.py
+++ Game.py
@@ -30,6 +30,9 @@ class Game:
         
         # updated actors
         self.update_actors(frame['updated_actors'])
+
+        if frame_index == 1000:
+            exit()
         
 
     
@@ -41,6 +44,7 @@ class Game:
     def delete_actors(self, actors):
         for actor in actors:
             actor_id = actor
+            # TODO: handle super-objects
             del self.actors[actor_id]
 
 
@@ -48,6 +52,9 @@ class Game:
         for actor in actors:
             actor_id = actor['actor_id']
             object_id = actor['object_id']
+
+            # add list of super-object_ids
+            actor['super_ids'] = []
 
             match object_id:
 
@@ -68,14 +75,25 @@ class Game:
 
             match actor['object_id']:
 
-                case 116: # TAGame.RBActor_TA:ReplicatedRBState (ReplicatedRigidBodyState)
-                    rigid_body_state = actor['attribute']['RigidBody']
-                    #for key in rigid_body_state:
-                    #    self.actors[actor_id][key] = rigid_body_state[key]
-                    self.actors[actor_id].update(rigid_body_state)
+                
 
                 case 16: # Engine.Actor:RemoteRole
                     self.actors[actor_id]['remote_role'] = actor['attribute']['Enum']
+
+                case 23: # TAGame.CarComponent_TA:Vehicle
+                    '''print(WARNING, self.object_lookup[self.actors[actor_id]['object_id']], end=' --> ')
+                    correspondig_id = actor['attribute']['ActiveActor']['actor']
+                    print(self.object_lookup[correspondig_id])
+                    print(OKGREEN, ' BASE:' + ENDC)
+                    pprint(self.actors[actor_id])
+                    print(OKGREEN, ' SUPER:' + ENDC)
+                    pprint(self.actors[correspondig_id])
+                    print(OKGREEN, ' ACTOR:' + ENDC)
+                    pprint(actor)'''
+                    # add actor_id to list of super-objects
+                    self.actors[actor_id]['super_ids'].append(actor['attribute']['ActiveActor']['actor'])
+                    
+                    
 
                 case 24: # TAGame.CarComponent_TA:ReplicatedActive
                     self.actors[actor_id]['active'] = actor['attribute']['Byte']
@@ -85,6 +103,12 @@ class Game:
                 
                 case 86: # TAGame.GameEvent_Soccar_TA:SecondsRemaining
                     self.actors[actor_id]['seconds_remaining'] = actor['attribute']['Int']
+                
+                case 116: # TAGame.RBActor_TA:ReplicatedRBState (ReplicatedRigidBodyState)
+                    rigid_body_state = actor['attribute']['RigidBody']
+                    #for key in rigid_body_state:
+                    #    self.actors[actor_id][key] = rigid_body_state[key]
+                    self.actors[actor_id].update(rigid_body_state)
 
                 case 123: # TAGame.Vehicle_TA:ReplicatedSteer
                     self.actors[actor_id]['steer'] = actor['attribute']['Byte']
@@ -93,7 +117,7 @@ class Game:
                     self.actors[actor_id]['throttle'] = actor['attribute']['Byte']
                 
                 case 126: # TAGame.Vehicle_TA:bReplicatedHandbrake
-                    self.actors[actor_id]['handbranke'] = actor['attribute']['Boolean']
+                    self.actors[actor_id]['handbrake'] = actor['attribute']['Boolean']
                 
                 case 156: # TAGame.CameraSettingsActor_TA:bUsingSecondaryCamera
                     self.actors[actor_id]['secondary_camera'] = actor['attribute']['Boolean']
@@ -101,12 +125,24 @@ class Game:
                 case 186: # Engine.PlayerReplicationInfo:Ping
                     self.actors[actor_id]['ping'] = actor['attribute']['Byte']
                 
+                case 193: # TAGame.PRI_TA:CurrentVoiceRoom
+                    self.actors[actor_id]['current_voice_room'] = actor['attribute']['String']
+                
                 case 234: # TAGame.PRI_TA:MatchScore
                     self.actors[actor_id]['score'] = actor['attribute']['Int']
                 
+                case 289: # TAGame.CarComponent_Dodge_TA:DodgeTorque
+                    self.actors[actor_id]['location'] = actor['attribute']['Location']
+                
+                case 346: # TAGame.VehiclePickup_TA:NewReplicatedPickupData
+                    self.actors[actor_id]['current_pickup'] = actor['attribute']['PickupNew'] | {'frame_index': self.time}
+            
+                
                 case _:
 
+                    pass
 
+                    
                     # this prints any other action that is not handled above
                     print(OKCYAN + f'Update Actor {actor_id} ({object_name} ({actor["object_id"]}))' + ENDC)
                     pprint(self.actors[actor_id])
